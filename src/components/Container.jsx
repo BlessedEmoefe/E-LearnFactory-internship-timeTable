4e98a0900e85cc73caa45e1335d3ff9fb2f4eb90
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-10-20 20:05:41 +0000

    my first commit,just created an empty react app and i'm about to startthe project

---
+++ src/components/Container.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Container extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>Hello World</p>
+            </div>
+         );
+    }
+}
+ 
+export default Container;
