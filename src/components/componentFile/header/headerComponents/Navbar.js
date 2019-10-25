06a04b4791371b4c6a183db5efc1e9d2c78585ad
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-10-25 22:58:31 +0000

    i just wrote my third react router code but it isn't working

--- src/components/componentFile/header/headerComponents/Navbar.js
+++ src/components/componentFile/header/headerComponents/Navbar.js
@@ -2,11 +2,20 @@ import React from 'react'
 import styles from "../../../styles/navbar.module.css"
 import RouterComponent from '../../routes/RouterComponent';
 
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    NavLink
+  } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
            <ul className={styles.ul}>  
-
            <RouterComponent />
            </ul>
 
