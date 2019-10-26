84338bae1279cebb9182fa83db39110100f93db6
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-10-26 22:21:56 +0000

    i just started working on the profile section

--- src/components/componentFile/body/profile/Profile.js
+++ src/components/componentFile/body/profile/Profile.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeaderContainer from '../../header/HeaderContainer';
 import Footer from '../../footer/Footer';
+import styles from "../../../styles/profile.module.css"
 
 const Profile = () => {
     return (
         <div>
             <HeaderContainer/>
+            <div className={styles.profile}>
+
             this is the Profile page
+            </div>
             <Footer/>
         </div>
     )
